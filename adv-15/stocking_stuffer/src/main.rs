d17afc6ed30d96a57de4265bc39dca8201b51613
irreal <milos.spasojevic.tms@gmail.com>
2020-03-02 22:14:11 +0100

    stocking stuffer

--- adv-15/stocking_stuffer/src/main.rs
+++ adv-15/stocking_stuffer/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let secret = "iwrupvqb";
+    let mut attempt = String::new();
+    let mut counter = 0;
+    while !attempt.starts_with("000000") {
+        let digest = md5::compute(&(format!("{}{}", &secret, &counter)));
+        attempt = format!("{:x}", digest);
+        counter += 1;
+    }
+    print!("The winning number is: {}", counter - 1);
 }
