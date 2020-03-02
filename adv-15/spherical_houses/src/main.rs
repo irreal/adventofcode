617fceb5dd615c78753335120761d8828e0b1ea0
irreal <milos.spasojevic.tms@gmail.com>
2020-03-02 20:29:15 +0100

    finish spherical houses, start stocking stuffer

--- adv-15/spherical_houses/src/main.rs
+++ adv-15/spherical_houses/src/main.rs
@@ -1,3 +1,45 @@
+use std::collections::HashSet;
+use std::fs;
+
+#[derive(Eq, Hash)]
+struct Coordinates {
+    x: i32,
+    y: i32,
+}
+impl PartialEq for Coordinates {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "input.txt";
+    let contents = fs::read_to_string(filename).expect("Couldn't read input file :(");
+    let mut visited: HashSet<Coordinates> = HashSet::new();
+    let mut santa_pos = Coordinates { x: 0, y: 0 };
+    let mut robot_pos = Coordinates { x: 0, y: 0 };
+    let mut santas_turn = true;
+    for d in contents.chars() {
+        visited.insert(Coordinates {
+            x: santa_pos.x,
+            y: santa_pos.y,
+        });
+        visited.insert(Coordinates {
+            x: robot_pos.x,
+            y: robot_pos.y,
+        });
+        let mover = match santas_turn {
+            true => &mut santa_pos,
+            false => &mut robot_pos,
+        };
+        match d {
+            'v' => (*mover).y -= 1,
+            '^' => (*mover).y += 1,
+            '<' => (*mover).x -= 1,
+            '>' => (*mover).x += 1,
+            _ => panic!("unexpected input character"),
+        };
+        santas_turn = !santas_turn;
+    }
+    print!("Total number of visited houses: {}", visited.len());
 }
