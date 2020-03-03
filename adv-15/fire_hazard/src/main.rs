b68624bf222458aa2ccf24b4eb23c0e0e996f042
irreal <milos.spasojevic.tms@gmail.com>
2020-03-03 23:25:17 +0100

    fire_hazard wip, bug present

---
+++ adv-15/fire_hazard/src/main.rs
@@ -0,0 +1,62 @@
+use std::fs;
+
+#[derive(Eq, Debug)]
+struct Coordinates {
+    x: usize,
+    y: usize,
+}
+
+impl PartialEq for Coordinates {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+
+fn main() {
+    let contents = fs::read_to_string("input.txt").expect("Couldn't read input file :(");
+    let lines = contents.lines();
+
+    let mut state = vec![vec![0usize; 1000]; 1000];
+
+    for line in lines {
+        let tokens: Vec<&str> = line.split(' ').collect();
+        let start_coord_token: Vec<&str> = tokens[1].split(',').collect();
+        let start_coord = Coordinates {
+            x: start_coord_token[0].parse::<usize>().unwrap(),
+            y: start_coord_token[1].parse::<usize>().unwrap(),
+        };
+
+        let end_coord_token: Vec<&str> = tokens[2].split(',').collect();
+        let end_coord = Coordinates {
+            x: end_coord_token[0].parse::<usize>().unwrap(),
+            y: end_coord_token[1].parse::<usize>().unwrap(),
+        };
+        for x in start_coord.x..(end_coord.x + 1) {
+            for y in start_coord.y..(end_coord.y + 1) {
+                state[x][y] = match tokens[0] {
+                    "on" => 1,
+                    "off" => 0,
+                    "toggle" => {
+                        if state[x][y] == 1 {
+                            1
+                        } else {
+                            0
+                        }
+                    }
+                    _ => panic!("Invalid operation found in input file!"),
+                };
+            }
+        }
+    }
+    let mut count = 0;
+    for x in 0..999 {
+        for y in 0..999 {
+            count += match state[x][y] {
+                0 => 0,
+                1 => 1,
+                _ => panic!("invalid value in state!"),
+            }
+        }
+    }
+    print!("Total on in the end {}", count);
+}
