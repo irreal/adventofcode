e07ae2481d21980adaeec3802f9dc91af0738081
Miloš Spasojević <milos.spasojevic.tms@gmail.com>
2020-03-06 22:51:35 +0100

    Getting close with assembly required

--- adv-15/assembly_required/src/main.rs
+++ adv-15/assembly_required/src/main.rs
@@ -1,4 +1,6 @@
+use std::collections::HashMap;
 use std::fs;
+
 
 #[derive(Debug)]
 enum Value {
@@ -18,30 +20,100 @@ enum Operation {
 
 
 fn main() {
+    let mut map = HashMap::<String, u16>::new();
+
     let contents = fs::read_to_string("input.txt").expect("Couldn't read input file :(");
     let lines = contents.lines();
+    let mut operations: Vec<Operation> = Vec::new();
     for line in lines {
         let tokens:Vec<&str> = line.split(' ').collect();
-        let operation = CreateOperation(tokens);
-        match operation {
-            Operation::Assign(v1,v2) => {print!("We got an assign! {:?} {:?}", v1, v2)},
-            _=> {print!("We aint got no assign here")}
+        let operation = create_operation(tokens);
+        operations.push(operation);
+
+    }
+    let mut executed_operation = true;
+    while executed_operation {
+        executed_operation = false;
+        for operation in &mut operations {
+        perform_operation(operation, &mut map);
         }
     }
 }
 
-fn CreateOperation(tokens: Vec<&str>) -> Operation {
-    if tokens.len() == 3 {
-     return Operation::Assign(CreateValue(tokens[0].to_string()), CreateValue(tokens[2].to_string()));   
+fn create_operation(tokens: Vec<&str>) -> Operation {
+    match tokens.len() {
+        3=> Operation::Assign(create_value(tokens[0].to_string()), create_value(tokens[2].to_string())),
+        4=> Operation::Not(create_value(tokens[1].to_string()), create_value(tokens[3].to_string())),
+        5=> {
+            match tokens[1] {
+                "AND" => Operation::And(create_value(tokens[0].to_string()), create_value(tokens[2].to_string()), create_value(tokens[4].to_string())),
+                "OR" => Operation::Or(create_value(tokens[0].to_string()), create_value(tokens[2].to_string()), create_value(tokens[4].to_string())),
+                "LShift" => Operation::LShift(create_value(tokens[0].to_string()), create_value(tokens[2].to_string()), create_value(tokens[4].to_string())),
+                "RShift" => Operation::RShift(create_value(tokens[0].to_string()), create_value(tokens[2].to_string()), create_value(tokens[4].to_string())),
+                _ => panic!("Unsupported operation! {}", tokens[1])
+            }
+        },
+        _=> panic!("Unsupported token length! {}", tokens.len())
     }
-    panic!("panika!");
 }
 
 
 
-fn CreateValue(value : String) -> Value {
+fn create_value(value : String) -> Value {
     if value.chars().nth(0).unwrap().is_numeric() {
-        return Value::Literal(16);
+        return Value::Literal(value.parse::<u16>().unwrap());
     }
     return Value::Reference(value);
+}
+
+fn get_value(value: Value, map: &HashMap::<String, u16>) -> Option<u16> {
+    match value {
+        Value::Literal(l) => Some(l),
+        Value::Reference(r) => {
+           match map.get(&r) {
+               Some(i) => Some(*i),
+               None => None
+           }
+        }
+    }
+}
+fn get_reference(value: Value) -> String {
+    match value {
+        Value::Reference(r) => r,
+        _=> panic!("Cannot get reference of literal value!")
+    }
+}
+
+fn perform_operation(operation: &mut Operation, map: &mut HashMap::<String, u16>) {
+    let target:Value;
+    let result = match operation {
+        Operation::Assign(v1,t) => {
+            target = t;
+            get_value(v1, &map).unwrap()
+        }
+        Operation::And(v1,v2,t) => {
+            target = t;
+            get_value(v1, &map).unwrap() & get_value(v2, &map).unwrap()
+        },
+        Operation::Or(v1,v2,t) => {
+            target = t;
+            get_value(v1, &map).unwrap() | get_value(v2, &map).unwrap()
+        },
+        Operation::Not(v1,t) => {
+            target = t;
+            !get_value(v1, &map).unwrap()
+        },
+        Operation::LShift(v1,v2,t) => {
+            target = t;
+            get_value(v1,&map).unwrap() << get_value(v2, &map).unwrap()
+        },
+        Operation::RShift(v1,v2,t) => {
+            target = t;
+            get_value(v1,&map).unwrap() >> get_value(v2, &map).unwrap()
+        },
+        _ => panic!("unsupported operation!")
+    };
+    let value = map.entry(get_reference(target)).or_insert(0);
+    *value = result;
+    println!("we did it! {:?}", map);
 }
